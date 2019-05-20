e6860d4a17b1286d149f52c73c055b33bfbca606
Serhii Behma <sbehm@softserveinc.com>
2019-05-20 12:23:13 +0300

    filters temp

--- src/app/module-group/components/group-grid/group-grid.component.ts
+++ src/app/module-group/components/group-grid/group-grid.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Language } from '../../models/language';
+import { LanguageService } from '../../services/language.service';
+import { AuthService } from '../../../module-account/services/auth/auth.service';
 
 @Component({
   selector: 'app-group-grid',
@@ -9,16 +12,22 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 export class GroupGridComponent implements OnInit {
   @Input() groupData: Array<any>;
   @Output() removeClicked = new EventEmitter<any>();
-  @Output() createClicked = new EventEmitter<any>();
   @Output() editClicked = new EventEmitter<any>();
+  @Output() applyFilters = new EventEmitter<any>();
+
+  languages: Language[];
+  filters: [string, string];
 
   public displayedColumns: string[];
 
-  constructor() {
+  constructor(private languageService: LanguageService, private authService: AuthService) {
     this.displayedColumns = ["date", "name", "language", "description", "picture", "edit", "delete"];
   }
 
   ngOnInit() {
+    this.languageService.getAll(this.authService.authorizationHeaderValue).subscribe((response: any) => {
+      this.languages = response;
+    });
   }
 
   public deleteRecord(record) {
@@ -28,14 +37,20 @@ export class GroupGridComponent implements OnInit {
   public editRecord(record) {
     const clonedRecord = Object.assign({}, record);
     this.editClicked.emit(clonedRecord);
-
-  }
-
-  public newRecord() {
-    this.createClicked.emit();
   }
 
   trackByFn(index, item) {
     return item.id; // уникальный id, соответствующий элементу
+  }
+
+  applyFilter(filter) {
+    if(this.filters.find(value => value == filter.name)){
+      this.filters[filter.name] = filter.value;      
+    }
+    else{
+      this.filters.join(filter.value);      
+    }
+    //  = filterValue.trim().toLowerCase();
+    this.applyFilters.emit(this.filters);   
   }
 }
