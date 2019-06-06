70c95e29ffa6cc8346388a88739b5112e23d54d7
Serhii Behma <sbehm@softserveinc.com>
2019-06-06 10:50:10 +0300

    add auth code flow

--- src/app/module-account/services/auth/auth.service.ts
+++ src/app/module-account/services/auth/auth.service.ts
@@ -78,7 +78,7 @@ export class AuthService extends BaseService {
       authority: authApiURI,
       redirect_uri: originUri + '/auth-callback',
       post_logout_redirect_uri: originUri,
-      response_type: "id_token token",
+      response_type: "code",
       scope: "openid profile email resourceapi",
       filterProtocolClaims: true,
       loadUserInfo: true,
